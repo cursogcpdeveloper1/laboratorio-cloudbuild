16efd4c62b26ee69a814884645f0b4946d93fe84
Miguel Leiva <migueleiva@gmail.com>
2025-07-08 23:50:26 +0000

    init commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+app.get('/', (req, res) => res.send('¡Hola desde Cloud Build + Cloud Run!'));
+app.listen(port, () => console.log(`App corriendo en el puerto ${port}`));
