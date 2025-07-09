082574487b47b2dc4d262d00d11247710d873374
Miguel Leiva <migueleiva@gmail.com>
2025-07-09 00:57:05 +0000

    Estoy cambiando el index.js2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-app.get('/', (req, res) => res.send('¡Hola desde Cloud Build + Cloud Run, modificando desde git!'));
+app.get('/', (req, res) => res.send('¡Hola desde Cloud Build + Cloud Run, modificando desde git2!'));
 app.listen(port, () => console.log(`App corriendo en el puerto ${port}`));
